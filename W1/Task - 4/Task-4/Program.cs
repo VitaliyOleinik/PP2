8681ce4dc2cbb554f04f4213ac6fabbca27374e3
VitaliyOleinik <oleynik624@gmail.com>
2019-01-30 23:31:02 +0600

    Task-4

---
+++ W1/Task - 4/Task-4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // created value(int) and convert it to int from string
+            int n = int.Parse(Console.ReadLine());
+            // created an array sT and set size n*n 
+            int[,] starTriangle = new int[n, n];
+            // created a loop from 0 to size of line array
+            for(int i = 0; i < n; i++)
+            {
+                // created a loop from 0 to size of line + 1
+                for (int j = 0; j < i + 1; j++)
+                {
+                    // output [*]
+                    Console.Write("[*]");
+
+                }
+                // new line
+                Console.WriteLine();
+            }
+            // created for cmd, will close after click any key (cmd)
+            Console.ReadKey();
+        }
+    }
+}
